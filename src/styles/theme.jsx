7581e780747103dbd9521d21a82a0ee21304bcc0
Jesus Calles <102770835+YiMonk@users.noreply.github.com>
2024-06-24 17:04:19 -0400

    Update theme.jsx

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -6,7 +6,7 @@ export const v = {
   color4: "#4A5C6A",
   color5: "#9BA8AB",
   color6: "#CCD0CF",
-  color7: "rgba(217, 217, 217, 0.28)",
+  color9: "rgba(217, 217, 217, 0.28)",
 
   color7: "#75D966",
   color8: "rgb(52, 131, 235)",

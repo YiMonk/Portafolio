d6223d8ae9e38904f13bf796d5cefc134c400e04
Jesus Calles <102770835+YiMonk@users.noreply.github.com>
2024-07-05 15:37:30 -0400

    responsive

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
   plugins: [react()]
+=======
+  plugins: [react()],
+  base:'https://yimonk.github.io/Portafolio'
+>>>>>>> 9e73faf (primera version del portafolio)
 })
